d3e0ee02c18554d09538ac6d1f717482f18e9550
Lior Shtaimberg <liorsht@wix.com>
2022-04-07 04:01:03 +0300

    working chart with expense data

--- house/views.py
+++ house/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from house.models import Country, House, City
+from expenses.models import Expenses
 from django.shortcuts import render
+from django.db.models import Sum
+from django.utils import timezone
 
 
 def home_page(request):
@@ -9,7 +12,7 @@ def home_page(request):
 
 def global_page(request):
     country = Country.objects.get(name="Israel")
-    House.create_house(
+    house = House.create_house(
         'House 1',
         True,
         Country.objects.get(name="Israel"),
@@ -19,8 +22,19 @@ def global_page(request):
         100,
         1,
     )
+
+    Expenses.create_expense(
+        house_name=house, amount='2500', date=timezone.now(), category='Rent', description="description"
+    )
+
     all_houses = House.objects.all()
-    context = {'all_houses': all_houses}
+    categories_amounts = Expenses.objects.order_by().values('category').annotate(total=Sum("amount"))
+
+    context = {
+        'all_houses': all_houses,
+        "categories": [category.get('category') for category in categories_amounts],
+        "amounts": [amount.get('total') for amount in categories_amounts],
+    }
     return render(request, 'global.html', context)
 
 
